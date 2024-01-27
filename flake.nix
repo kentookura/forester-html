43f1a69afcf952fbe81e9b4eba29806884540569
Kento Okura <kentookura@gmail.com>
2024-01-27 10:41:25 +0100

    add stuff to flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,24 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    forester.url = "sourcehut:~jonsterling/ocaml-forester";
+    forest-server.url = "github:kentookura/forest-server";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
+  outputs = { self, nixpkgs, utils, forester, forest-server }:
     utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in {
         devShell = with pkgs;
           mkShell {
-            buildInputs = [ libev opam fswatch pkg-config openssl];
+            buildInputs = [
+              libev
+              opam
+              ocamlPackages.odoc
+              forester.packages.${system}.default
+              forest-serer.packages.${system}.default
+            ];
             shellHook = "eval $(opam env --switch=5.1.1)";
           };
       });
